c34813840c979ed7b580e6a970b2ebb869fd180e
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-11-15 13:43:06 +0100

    Update 2022_11_15_122957_create_projects_table.php

--- database/migrations/2022_11_15_122957_create_projects_table.php
+++ database/migrations/2022_11_15_122957_create_projects_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('name');
+            $table->string('description');
+            $table->bigInteger('goal');
+            $table->bigInteger('progress');
             $table->timestamps();
         });
     }
