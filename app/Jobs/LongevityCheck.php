50c398f5754113222e53671e66edf41bd85eee76
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-12-05 14:38:18 +0100

    longevity

---
+++ app/Jobs/LongevityCheck.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Project;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class LongevityCheck implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        foreach (Project::all() as $project)
+        {
+            $date = Carbon::now()
+                ->diffInWeeks(
+                    Carbon::parse($project->created_at));
+
+            if ($date > 14) Project::destroy($project->id);
+        }
+    }
+}
