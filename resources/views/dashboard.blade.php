d1cb7636bda646b5b598f8a688f965a23bb970c1
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-12-06 11:04:14 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,10 +17,13 @@
                             <progress id="file" value="{{$project->progress}}" max="{{$project->goal}}"></progress>
                             <p>{{$project->goal}}</p>
                         </div>
-                        <div>
-                            <a href="{{route('deposit',$project->id)}}" class="">
-                                <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.121 15.536c-1.171 1.952-3.07 1.952-4.242 0-1.172-1.953-1.172-5.119 0-7.072 1.171-1.952 3.07-1.952 4.242 0M8 10.5h4m-4 3h4m9-1.5a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>                            </a>
-                        </div>
+                        @if($project->user_id != Auth::user()->id)
+                            <div>
+                                <a href="{{route('deposit',$project->id)}}" class="">
+                                    <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.121 15.536c-1.171 1.952-3.07 1.952-4.242 0-1.172-1.953-1.172-5.119 0-7.072 1.171-1.952 3.07-1.952 4.242 0M8 10.5h4m-4 3h4m9-1.5a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                </a>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
