ed1397cdea759ee5e678336d747b779daeffb554
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-12-08 10:56:07 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <a href="{{route("projects.show", $project->id)}}">{{$project->name}}</a>
+                        <a href="{{route("project.show", $project->id)}}">{{$project->name}}</a>
                         <p>{{$project->user->name}}</p>
                         <p>{{$project->description}}</p>
                         <div class="flex flex-row">
